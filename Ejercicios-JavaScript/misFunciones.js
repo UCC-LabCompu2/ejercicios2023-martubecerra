ad257839ce975ff960ae64e5241913227c1675d1
martubecerra <2214822@ucc.edu.ar>
2023-07-25 23:22:49 -0300

    ejercicios

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,7 +1,123 @@
 /**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
+ * Conversion de unidades (metros, pulgadas, pies y yardas)
+ * @method cambiarUnidades
+ * @param {string} id - id de los inputs de metros, pulgadas, pies o yardas
+ * @param {number} valor - valor de los inputs de metros, pulgadas, pies o yardas
+ * @return
  */
+function cambiarUnidades (id, valor){
+        let met, pul, pie, yar;
+
+        if(valor.includes(",")){
+            valor=valor.replace (",", ".");
+        }
+        if (isNaN(valor)){
+            alert("El valor ingresado es incorrecto");
+            met = " ";
+            pul= " ";
+            pie= " ";
+            yar= " ";
+        } else if (id=="metro"){
+            met=valor;
+            pul= valor*39.3701;
+            pie= valor * 3.28084;
+            yar= valor * 1.09361;
+        } else if (id=="pulgada") {
+            pul=valor;
+            met = valor * 0.0254;
+            pie = valor * 0.08333;
+            yar = valor * 0.027778;
+        } else if (id=="pie") {
+            pie=valor;
+            met = valor * 0.3048;
+            pul = valor * 12;
+            yar = valor * 0.333333;
+        } else if (id=="yarda") {
+            yar=valor;
+            met = valor * 0.9144;
+            pul = valor * 36;
+            pie = valor * 3;
+        }
+        document.lasUnidades.unid_metro.value =Number(met).toFixed(2);
+        document.lasUnidades.unid_pulgada.value= Number(pul).toFixed(2);
+        document.lasUnidades.unid_pie.value= Number(pie).toFixed(2);
+        document.lasUnidades.unid_yarda.value= Number(yar).toFixed(2);
+}
+
+function gradosYradianes(id) {
+    var grad, rad;
+    if (id=="grados"){
+        grad = document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180;
+    } else if (id=="radianes"){
+        rad = document.getElementById("radianes").value;
+        grad = (rad*180)/Math.PI;
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
+
+function mostrar_ocultar(valorMO) {
+    if (valorMO=="val_mostrar"){
+        document.getElementById("divMO").style.display = 'block';
+    } else if (valorMO=="val_ocultar") {
+        document.getElementById("divMO").style.display = 'none';
+    }
+}
+
+function calcularSuma() {
+    var num1, num2;
+
+    num1 = Number(document.getElementsByName("sum_num1")[0].value);
+    num2 = Number(document.getElementsByName("sum_num2")[0].value);
+
+    document.getElementsByName("sum_total")[0].innerHTML = num1 + num2;
+}
+
+function calcularResta() {
+    var num1, num2;
+
+    num1 = Number(document.getElementsByName("res_num1")[0].value);
+    num2 = Number(document.getElementsByName("res_num2")[0].value);
+
+    document.getElementsByName("res_total")[0].innerHTML = num1 - num2;
+}
+
+function calcularMultiplicacion() {
+    var num1, num2;
+
+    num1 = Number(document.getElementsByName("mul_num1")[0].value);
+    num2 = Number(document.getElementsByName("mul_num2")[0].value);
+
+    document.getElementsByName("mul_total")[0].innerHTML = num1 * num2;
+}
+
+function calcularDivision() {
+    var num1, num2;
+
+    num1 = Number(document.getElementsByName("div_num1")[0].value);
+    num2 = Number(document.getElementsByName("div_num2")[0].value);
+
+    document.getElementsByName("div_total")[0].innerHTML = num1 / num2;
+}
+
+function cargarWeb() {
+    var cant, unidad, urlComp;
+
+    cant = document.getElementById("distancia").value;
+    unidad = document.getElementsByName("unidades")[0].value;
+
+    urlComp = "segundaWeb.html#" + cant + "#" + unidad;
+    window.open(urlComp)
+}
+
+function cargarResultado() {
+    var urlComp, can, un;
+
+    urlComp = window.location.href.split("/")[5];
+
+    can = urlComp.split("#")[1];
+    un = urlComp.split("#")[2];
+
+    document.getElementById("dist").value = can + " " + un;
+}
